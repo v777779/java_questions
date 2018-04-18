eed9e2cdcc0549f7845f84fb901c03d1fb56abbe
Vadim Voronov <vadim.v.voronov@gmail.com>
2018-04-18 21:46:55 +0300

    project update JMX Model MBean

--- src/java02/jmxmodel/jmxm/MainStart.java
+++ src/java02/jmxmodel/jmxm/MainStart.java
@@ -1,5 +1,7 @@
 package java02.jmxmodel.jmxm;
 
+import java.io.IOException;
+
 import static java02.jmxmodel.jmxm.ModelMBeanDemonstrator.pause;
 
 /**
@@ -12,6 +14,13 @@ public class MainStart {
     public static void main(String[] args) {
         System.out.println("Run MainStart ");
         System.out.println("Run JConsole");
+        System.out.println("JConsole >> MBeans >> modelmbean");
+        try {
+            Runtime.getRuntime().exec("jconsole");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         final ModelMBeanDemonstrator me = new ModelMBeanDemonstrator();
         me.createAndRegisterRawModelMBean();
         pause(1000000);
