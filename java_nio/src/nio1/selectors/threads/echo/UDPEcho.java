2f178c50828dd72ad42c3b51ab648fb4a139a7f7
Vadim Voronov <vadim.v.voronov@gmail.com>
2018-07-05 14:07:55 +0300

    project update nio1 udp echo server added

--- java_nio/src/nio1/selectors/threads/echo/UDPEcho.java
+++ java_nio/src/nio1/selectors/threads/echo/UDPEcho.java
@@ -9,6 +9,8 @@ import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 import java.nio.charset.Charset;
 import java.util.Arrays;
@@ -24,9 +26,12 @@ import java.util.stream.Collectors;
  */
 public class UDPEcho {
     private static final Charset TELNET_CHARSET = Charset.forName("CP866");
+    private static final Charset UDP_CHARSET = Charset.forName("UTF-8");
+
     private static final String HOST = "localhost";
     private static final int PORT = 9990;
-    private static final int PORT_UDP = 9910;
+    private static final int UDP_PORT = 9910;
+    private static final int UDP_PORT2 = 9910;
 
     private static final String[] PUTTY_WELCOME = {
             "Welcome to echo server!",
@@ -56,7 +61,7 @@ public class UDPEcho {
         return isBlocking;
     }
 
-// runners
+    // runners
     public static void runTelnet(String host, int port) throws IOException {
         Runtime.getRuntime().exec("cmd /c start telnet " + host + " " + port);
     }
@@ -68,60 +73,172 @@ public class UDPEcho {
 
     public static void main(String[] args) {
 
-
         System.out.printf("udp server started...%n");
+        DatagramChannel dc = null;
         try {
-            runPutty("localhost", 9990);
             ExecutorService exec = Executors.newCachedThreadPool();
-            exec.execute(new InputServer());
-            exec.shutdown();
-// udp server
+            runPutty(HOST, PORT);
+            exec.execute(new InputServer(UDP_CHARSET, PORT, UDP_PORT));  // for putty
 
+//            runTelnet(HOST, PORT);
+//            exec.execute(new InputServer(TELNET_CHARSET, PORT, UDP_PORT);  // for telnet
+
+            exec.shutdown();
+
+// udp server
+            InetSocketAddress inetSocketAddress = new InetSocketAddress(HOST, UDP_PORT);
+            dc = DatagramChannel.open();
+            dc.socket().bind(inetSocketAddress); // listening port
+            dc.configureBlocking(false);
+
+            ByteBuffer b = ByteBuffer.allocate(1024);
 
             while (true) {
                 if (exec.isTerminated()) {
                     break;
                 }
+                b.clear();
+                SocketAddress socketAddress = dc.receive(b);
+                if (socketAddress != null) {
+                    b.flip();
+                    String s = "UDP:" + new String(b.array(), 0, b.limit(), UDP_CHARSET);
+
+                    b.clear();
+                    b.put(s.getBytes(UDP_CHARSET));
+                    b.flip();
+                    dc.send(b, socketAddress); // не постоянное соединение, поэтому всегда отвечаем адресату
+
+                }
+                Thread.sleep(100);
             }
 
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            IOUtils.close(dc);
         }
 
         System.out.printf("udp server closed...%n");
     }
 
 
-    private static class InputServer implements Runnable {
+//    private static class UDPServer implements Runnable {
+//        private final boolean isTelnet;
+//        private final int port;
+//        private final int udpPort;
+//
+//
+//        public UDPServer(boolean isTelnet, int serverPort) {
+//            this.isTelnet = isTelnet;
+//        }
+//
+//        @Override
+//        public void run() {
+//            System.out.printf("udp server started...%n");
+//            DatagramChannel dc = null;
+//
+//            try {
+//                ExecutorService exec = Executors.newCachedThreadPool();
+//                if (isTelnet) {
+//                    runTelnet(HOST, PORT);
+//                    exec.execute(new InputServer(TELNET_CHARSET, PORT, UDP_PORT));  // for telnet
+//                } else {
+//                    runPutty(HOST, PORT);
+//                    exec.execute(new InputServer(UDP_CHARSET, PORT, UDP_PORT));  // for putty
+//                }
+//
+//                exec.shutdown();
+//// udp server
+//                InetSocketAddress inetSocketAddress = new InetSocketAddress(HOST, UDP_PORT);
+//                dc = DatagramChannel.open();
+//                dc.socket().bind(inetSocketAddress); // listening port
+//                dc.configureBlocking(false);
+//
+//                ByteBuffer b = ByteBuffer.allocate(1024);
+//
+//                while (true) {
+//                    if (exec.isTerminated()) {
+//                        break;
+//                    }
+//                    b.clear();
+//                    SocketAddress socketAddress = dc.receive(b);
+//                    if (socketAddress != null) {
+//                        b.flip();
+//                        String s = "UDP:" + new String(b.array(), 0, b.limit(), UDP_CHARSET);
+//
+//                        b.clear();
+//                        b.put(s.getBytes(UDP_CHARSET));
+//                        b.flip();
+//                        dc.send(b, socketAddress); // не постоянное соединение, поэтому всегда отвечаем адресату
+//
+//                    }
+//                    Thread.sleep(100);
+//                }
+//
+//            } catch (IOException | InterruptedException e) {
+//                e.printStackTrace();
+//            } finally {
+//                IOUtils.close(dc);
+//            }
+//
+//            System.out.printf("udp server closed...%n");
+//
+//        }
+//    }
 
+    private static class InputServer implements Runnable {
+        private BufferedReader br;
+        private PrintWriter pw;
+        private Charset consoleCharset;
+        private int port;
+        private int udpPort;
+
+        public InputServer(Charset consoleCharset, int port, int udpPort) {
+            this.consoleCharset = consoleCharset;
+            this.port = port;
+            this.udpPort = udpPort;
+        }
 
         @Override
         public void run() {
-
+// socket
             ServerSocket ssc = null;
             Socket sc = null;
-            BufferedReader br = null;
-            PrintWriter pw = null;
             System.out.printf("socket server started...%n");
-
+// udp
             DatagramChannel dc = null;
+            ByteBuffer b = ByteBuffer.allocate(1024);
+            InetSocketAddress udpAddress = new InetSocketAddress(HOST, udpPort);
+
             try {
                 ssc = new ServerSocket();
-                ssc.bind(new InetSocketAddress(HOST, PORT));
+                ssc.bind(new InetSocketAddress(HOST, port));
 
                 sc = ssc.accept();
-                br = new BufferedReader(new InputStreamReader(sc.getInputStream(), TELNET_CHARSET));
-                pw = new PrintWriter(sc.getOutputStream(), true, TELNET_CHARSET); // flush by /r/n
+                br = new BufferedReader(new InputStreamReader(sc.getInputStream(), consoleCharset));
+                pw = new PrintWriter(sc.getOutputStream(), true, consoleCharset); // flush by /r/n
                 pw.println(Arrays.stream(PUTTY_WELCOME).collect(Collectors.joining(String.format("%n"))));
 
-
+//udp
                 dc = DatagramChannel.open();
-                dc.socket().bind(new InetSocketAddress(HOST, PORT_UDP));
+                dc.connect(udpAddress);
 
                 while (true) {
                     String s;
                     if ((s = br.readLine()) == null) break;  // client closed
+// udp
+                    b.clear();
+                    b.put(s.getBytes(UDP_CHARSET));
+                    b.flip();
+                    dc.write(b);                        //  dc.send(b,udpAddress);
+
+
+                    b.clear();
+                    dc.receive(b);
+                    b.flip();
+                    s = new String(b.array(), 0, b.limit(), UDP_CHARSET);
                     pw.println(s);
+
 
                 }
 
